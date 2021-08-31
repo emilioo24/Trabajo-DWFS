4c4c66f3504cd64ec924dca27a4e7d7d1aaeb2f8
emilioo24 <lobocalderon@outlook.es>
2021-08-31 13:24:35 -0300

    Arreglado y mejorado

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const sqlcon = require('../utils/conexion');
 var express = require('express');
 var router = express.Router();
+var coments = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -24,24 +25,50 @@ router.get('/contacto', function (req, res) {
 });
 
 router.post('/enviar', function (req, res) {
-  console.log(req.body);
   const user = req.body;
-  
-  sqlcon.query(`INSERT INTO users(nombre, email, telefono, mensaje) 
-  VALUES ('${user.nombre}', '${user.email}', '${user.telefono}', '${user.mensaje}')`, function (error, result) {
-    if (error) {
-      console.error(error);
-      return res.render('contacto', { mensaje: "Error al enviar el mensaje", color: "red" });
+  if (user.nombre === "") {
+    return res.render('contacto', { mensaje: "Debe completar el campo nombre", color: "red" });
+  } else if (user.email === "") {
+    return res.render('contacto', { mensaje: "Debe completar el campo email", color: "red" });
+  } else if (user.mensaje === "") {
+    return res.render('contacto', { mensaje: "Debe completar el campo mensaje", color: "red" });
+  } else {
+    if (user.mensaje[0] !== "") {
+      user.mensaje = user.mensaje[0];
+    } else if (user.mensaje[1] !== "") {
+      user.mensaje = user.mensaje[1];
+    } else {
+      user.mensaje = user.mensaje[2];
     }
-    res.render('contacto', { mensaje: "Mensaje enviado con éxito", color: "green" });
-    console.log(result);
-  });
-
+    sqlcon.query(`INSERT INTO users(nombre, email, telefono, mensaje) 
+    VALUES ('${user.nombre}', '${user.email}', '${user.telefono}', '${user.mensaje}')`, function (error, result) {
+      if (error) {
+        console.error(error);
+        return res.render('contacto', { mensaje: "Error al enviar el mensaje", color: "red" });
+      }
+      res.render('contacto', { mensaje: "Mensaje enviado con éxito", color: "green" });
+    });
+  }
 });
 
-
-router.get('/api', function (req, res) {
-  res.json({ message: "" });
+router.get('/admin', function (req, res) {
+  sqlcon.query(`SELECT * FROM users`, function (error, result) {
+    if (error) {
+      console.error(error);
+    } else {
+      /*for (let i = 0; i < result.length; i++) {
+        console.log(result[i].nombre)
+        console.log(result[i].email)
+        console.log(result[i].telefono)
+        console.log(result[i].mensaje)
+        console.log("------------");
+      }*/
+      /*res.send(result.map(function(element) {
+        console.log(element.nombre);
+      }));*/
+      res.render('admin', { resultado: result });
+    }
+  });
 });
 
 module.exports = router;
--- utils/conexion.js
+++ utils/conexion.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql2');
+const { promisify } = require('util');
 const mysqlConfig = require('../config/config');
 
-const conexion = mysql.createConnection(mysqlConfig);
-conexion.connect(function (error, result) {
+const conexion = mysql.createPool(mysqlConfig);
+conexion.getConnection(function (error, result) {
   if (error) {
+    console.log("ERROR");
     console.error(error);
-    console.log('ERROR');
-    conexion.end();
   }
-
+  
   console.log('Conectado Correctamente');
 });
+
+conexion.query = promisify(conexion.query);
 
 module.exports = conexion;
